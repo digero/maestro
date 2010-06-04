585d213d1987e9af39a3a17dcd3e9f70b621d1ff
Ben <Ben@92cf9013-e6d5-78db-2e6a-d95f4190883e>
2010-06-04 06:40:59 +0000

    Merge drums into LotroInstruments.sf2

--- LotroMaestro/src/com/digero/maestro/midi/synth/SynthesizerFactory.java
+++ LotroMaestro/src/com/digero/maestro/midi/synth/SynthesizerFactory.java
@@ -3,21 +3,46 @@ package com.digero.maestro.midi.synth;
 import java.io.IOException;
 
 import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.MidiDevice;
 import javax.sound.midi.MidiSystem;
 import javax.sound.midi.MidiUnavailableException;
 import javax.sound.midi.Soundbank;
 import javax.sound.midi.Synthesizer;
+import javax.sound.midi.MidiDevice.Info;
+
+import com.sun.media.sound.AudioSynthesizer;
 
 public class SynthesizerFactory {
 	private static Soundbank lotroSoundbank;
-	private static Soundbank lotroDrumbank;
 
 	public static Synthesizer getLotroSynthesizer() throws MidiUnavailableException, InvalidMidiDataException,
 			IOException {
+		Synthesizer synth = MidiSystem.getSynthesizer();
+		if (synth != null)
+			initLotroSynthesizer(synth);
+		return synth;
+	}
+
+	public static AudioSynthesizer getLotroAudioSynthesizer() throws MidiUnavailableException,
+			InvalidMidiDataException, IOException {
+		AudioSynthesizer synth = findAudioSynthesizer();
+		if (synth != null)
+			initLotroSynthesizer(synth);
+		return synth;
+	}
+
+	public static void initLotroSynthesizer(Synthesizer synth) throws MidiUnavailableException,
+			InvalidMidiDataException, IOException {
+		getLotroSoundbank();
+		synth.open();
+		synth.unloadAllInstruments(lotroSoundbank);
+		synth.loadAllInstruments(lotroSoundbank);
+	}
+
+	public static Soundbank getLotroSoundbank() throws InvalidMidiDataException, IOException {
 		if (lotroSoundbank == null) {
 			try {
 				lotroSoundbank = MidiSystem.getSoundbank(SynthesizerFactory.class.getResource("LotroInstruments.sf2"));
-				lotroDrumbank = MidiSystem.getSoundbank(SynthesizerFactory.class.getResource("LotroDrums.sf2"));
 			}
 			catch (NullPointerException npe) {
 				// JARSoundbankReader throws a NullPointerException if the file doesn't exist
@@ -31,12 +56,27 @@ public class SynthesizerFactory {
 				}
 			}
 		}
+		return lotroSoundbank;
+	}
+
+	/*
+	 * Find available AudioSynthesizer.
+	 */
+	public static AudioSynthesizer findAudioSynthesizer() throws MidiUnavailableException {
+		// First check if default synthesizer is AudioSynthesizer.
 		Synthesizer synth = MidiSystem.getSynthesizer();
-		synth.open();
-		synth.unloadAllInstruments(lotroSoundbank);
-		synth.loadAllInstruments(lotroSoundbank);
-		synth.unloadAllInstruments(lotroDrumbank);
-		synth.loadAllInstruments(lotroDrumbank);
-		return synth;
+		if (synth instanceof AudioSynthesizer)
+			return (AudioSynthesizer) synth;
+
+		// If default synhtesizer is not AudioSynthesizer, check others.
+		Info[] infos = MidiSystem.getMidiDeviceInfo();
+		for (int i = 0; i < infos.length; i++) {
+			MidiDevice dev = MidiSystem.getMidiDevice(infos[i]);
+			if (dev instanceof AudioSynthesizer)
+				return (AudioSynthesizer) dev;
+		}
+
+		// No AudioSynthesizer was found, return null.
+		return null;
 	}
 }
