16193d9710b4f55c5d8f413ce19796af5ff3b3b2
Ben <Ben@92cf9013-e6d5-78db-2e6a-d95f4190883e>
2013-07-20 23:15:55 +0000

    Add match directories flag to ExtensionFileFilter

--- LotroMaestro/trunk/src/com/digero/common/util/ExtensionFileFilter.java
+++ LotroMaestro/trunk/src/com/digero/common/util/ExtensionFileFilter.java
@@ -6,9 +6,15 @@ import java.util.regex.Pattern;
 public class ExtensionFileFilter extends javax.swing.filechooser.FileFilter implements java.io.FileFilter {
 	private Pattern fileNameRegex;
 	private String description;
+	private boolean matchDirectories;
 
 	public ExtensionFileFilter(String description, String... fileTypes) {
+		this(description, true, fileTypes);
+	}
+
+	public ExtensionFileFilter(String description, boolean matchDirectories, String... fileTypes) {
 		this.description = description;
+		this.matchDirectories = matchDirectories;
 
 		String regex = ".*\\.(" + fileTypes[0];
 		for (int i = 1; i < fileTypes.length; i++)
@@ -20,7 +26,7 @@ public class ExtensionFileFilter extends javax.swing.filechooser.FileFilter impl
 	@Override
 	public boolean accept(File f) {
 		if (f.isDirectory())
-			return true;
+			return matchDirectories;
 
 		return fileNameRegex.matcher(f.getName()).matches();
 	}
