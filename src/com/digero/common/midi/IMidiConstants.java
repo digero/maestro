6bb0e64fe64651bfa2817cbee26eaac46ea51738
Ben <Ben@92cf9013-e6d5-78db-2e6a-d95f4190883e>
2013-07-24 12:38:05 +0000

    The volume goes to eleven!

--- src/com/digero/common/midi/IMidiConstants.java
+++ src/com/digero/common/midi/IMidiConstants.java
@@ -32,7 +32,7 @@ public interface IMidiConstants {
 	public static final int DEFAULT_TEMPO_BPM = 120;
 	public static final int DEFAULT_TEMPO_MPQ = 500000;
 	public static final int DEFAULT_INSTRUMENT = 0;
-	public static final int DEFAULT_CHANNEL_VOLUME = MAX_VOLUME;
+	public static final int DEFAULT_CHANNEL_VOLUME = 100;
 	public static final int DEFAULT_PITCH_BEND_RANGE_SEMITONES = 2;
 	public static final int DEFAULT_PITCH_BEND_RANGE_CENTS = 0;
 }
