bf929259d9bade8cf8fc2eedae5a8f74a3865993
digero@gmail.com <digero@gmail.com@92cf9013-e6d5-78db-2e6a-d95f4190883e>
2013-08-25 21:24:20 +0000

    Update version location info

--- LotroMaestro/trunk/src/com/digero/abcplayer/AbcPlayer.java
+++ LotroMaestro/trunk/src/com/digero/abcplayer/AbcPlayer.java
@@ -109,7 +109,7 @@ public class AbcPlayer extends JFrame implements TableLayoutConstants, IMidiCons
 	private static final String APP_NAME_LONG = APP_NAME + " for The Lord of the Rings Online";
 	private static final String APP_URL = "http://lotro.acasylum.com/abcplayer/";
 	private static final String LAME_URL = "http://lame.sourceforge.net/";
-	private static Version APP_VERSION = new Version(1, 3, 1);
+	private static Version APP_VERSION = new Version(0, 0, 0);
 
 	private static AbcPlayer mainWindow = null;
 
