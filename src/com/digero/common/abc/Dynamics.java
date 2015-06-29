c87262ba73b99fbaa14925eacaa8488d501dbc15
digero <digero@users.noreply.github.com>
2015-06-29 05:47:37 -0700

    Update instrument volumes and dynamics to more closely match LOTRO

--- src/com/digero/common/abc/Dynamics.java
+++ src/com/digero/common/abc/Dynamics.java
@@ -5,16 +5,15 @@ import com.digero.common.util.Util;
 
 public enum Dynamics
 {
-	// In LotRO, +ppp+ seems to be 50% volume
-	pppp(4, 56), //
-	ppp(16, 64), //
-	pp(32, 72), //
-	p(48, 80), //
-	mp(64, 88), //
-	mf(80, 96), //
-	f(96, 104), //
-	ff(112, 112), //
-	fff(127, 120), //
+	pppp(4, 60), //
+	ppp(16, 61), //
+	pp(32, 75), //
+	p(48, 87), //
+	mp(64, 97), //
+	mf(80, 106), //
+	f(96, 114), //
+	ff(112, 122), //
+	fff(127, 126), //
 	ffff(144, 127);
 
 	public static final Dynamics DEFAULT = mf;
